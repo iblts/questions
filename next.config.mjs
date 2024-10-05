017fb2ba108f8831eeb4f1afa60717c233df7582
iblts <ibelyaevnmc@gmail.com>
2024-10-05 22:09:37 +0700

    Add webpack config

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,10 @@ const nextConfig = {
 		API_URL: 'http://localhost:3000/api',
 		SECRET_KEY: 'iblts',
 	},
+	webpack: config => {
+		config.externals.push('@node-rs/argon2', '@node-rs/bcrypt')
+		return config
+	},
 }
 
 export default nextConfig
