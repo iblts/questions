5c9cffb502c396062d5a0e2ffef364c21eff90b7
iblts <ibelyaevnmc@gmail.com>
2024-10-05 22:57:07 +0700

    Finally auth

--- src/features/auth/signOut.ts
+++ src/features/auth/signOut.ts
@@ -1,5 +1,26 @@
+import { lucia } from '@/lib/lucia'
+import { cookies } from 'next/headers'
+import { redirect } from 'next/navigation'
+import { getAuth } from './getAuth'
+
 export async function signOut() {
 	'use server'
 
-	console.log('sign out')
+	const { session } = await getAuth()
+
+	if (!session) {
+		redirect('/atuh/login')
+	}
+
+	await lucia.invalidateSession(session.id)
+
+	const sessionCookie = lucia.createBlankSessionCookie()
+
+	cookies().set(
+		sessionCookie.name,
+		sessionCookie.value,
+		sessionCookie.attributes
+	)
+
+	redirect('/auth/login')
 }
