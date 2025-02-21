df0e39986cb3809b4213587721af05208650e344
iblts <ibelyaevnmc@gmail.com>
2025-02-21 18:15:28 +0700

    Fix env

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
 	env: {
-		API_URL: 'http://localhost:3000/api',
+		API_URL: 'https://questions-blush.vercel.app/api/',
 		SECRET_KEY: 'iblts',
 	},
 }
